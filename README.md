47dad553a18c8b8a3ed6a614f18b59c0653add8b
Jens Berling <jens.lucas@gmail.com>
2025-12-26 12:53:12 +0100

    dec 11 2star

--- README.md
+++ README.md
@@ -2,24 +2,28 @@
 
 ## Progress
 
-|       | Stars         | Date |        | Stars         | Date         |
-| ----- | ------------- | ---- | ------ | ------------- | ------------ |
-| **1** | :star: :star: | 2/12 | **7**  | :star: :star: | 7/12         |
-| **2** | :star: :star: | 2/12 | **8**  | :star: :star: | 8/12         |
-| **3** | :star: :star: | 3/12 | **9**  | :star::star:  | 9/12 (23/12) |
-| **4** | :star: :star: | 4/12 | **10** | :star:        | 10/12        |
-| **5** | :star: :star: | 5/12 | **11** | :star:        |              |
-| **6** | :star: :star: | 6/12 | **12** | :star:        | 15/12        |
+|       | Stars         | Date |        | Stars         | Date          |
+| ----- | ------------- | ---- | ------ | ------------- | ------------- |
+| **1** | :star: :star: | 2/12 | **7**  | :star: :star: | 7/12          |
+| **2** | :star: :star: | 2/12 | **8**  | :star: :star: | 8/12          |
+| **3** | :star: :star: | 3/12 | **9**  | :star: :star: | 9/12 (23/12)  |
+| **4** | :star: :star: | 4/12 | **10** | :star:        | 10/12         |
+| **5** | :star: :star: | 5/12 | **11** | :star: :star: | 11/12 (26/12) |
+| **6** | :star: :star: | 6/12 | **12** | :star:        | 15/12         |
 
 For the time being it seems I've hit a stop on december 10th and 11th. I have ideas on how to solve both but...
 
-# December 9
+## December 9
 
 Took me so many reworks, hahah. In the end I am proud of my solution. The map is reduced to a much smaller map by removing every empty row and column. This then becomes a managable size for some simple numpy compare.
 
 The reduced area can be seen bellow, including the best rectangle. Notice the huge spike in the middle, probably killing most other good rectangles.
 ![best area on reduced map](assets/dec9.png)
 
-# December 12
+## Decamber 11
+
+Just have to polish upp my graph algoithms.
+
+## December 12
 
 I am masively disapointed in this star :|. Before coding I made a sanity check to see how many shapes where under/equal/over the given area. I found about half where bellow. I was ready to start coding a few days later when I thought I should make sure the answer wasn't simply every area where it theoreticaly could fit... but it was.
--- src/dec11.py
+++ src/dec11.py
@@ -2,6 +2,15 @@ from utils.file import get_input_list
 
 PATH = "inputs/dec11.txt"
 
+""" Insights for part 2
+
+- No cycles
+- Part 1 required only nodes single visit, dirived from no cycles
+
+"""
+
+# Utils -----
+
 
 def build_graph(lines):
 
@@ -24,6 +33,75 @@ def build_graph(lines):
     return graph
 
 
+# Never used, had an idea of the input data
+def invert_graph(org_graph):
+
+    new_graph = {}
+
+    for end, starts in org_graph.items():
+        for start in starts:
+            new_graph[start] = new_graph.get(start, [])
+            new_graph[start].append(end)
+
+    return new_graph
+
+
+# Used for sanity check
+def has_cycle(graph, node, visited, rec_stack):
+    """Debug function to test if it has a cycle in it"""
+
+    if node in rec_stack:
+        print(f"Node {node} found in {rec_stack}")
+        return True
+
+    if node in visited:
+        return False
+
+    nexts = graph.get(node, [])
+
+    visited.add(node)
+    rec_stack.add(node)
+
+    for next in nexts:
+        if has_cycle(graph, next, visited, rec_stack):
+            return True
+
+    rec_stack.remove(node)
+    return False
+
+
+def topological_sort(graph):
+    """
+
+    Sorts all nodes in topological order
+
+    Using Khan's algorithm as described in wikipedia
+    """
+
+    L = []  # ordered list of nodes in topological order
+    i_graph = invert_graph(graph)
+    S = graph.keys() - i_graph.keys()  # Set of all nodes with no incoming edges
+    visited = set()  # All nodes processed
+
+    while len(S) > 0:
+        n = S.pop()
+        visited.add(n)
+        L.append(n)
+
+        others = graph.get(n, [])  # Nodes that connect to
+
+        for m in others:
+            starts = set(i_graph[m])  # Other nodes that connect to m
+            if starts & visited == starts:  # If all previous nodes have been vissited
+                S.add(m)
+
+    return L
+
+
+# Solution ----
+
+
+# One star
 def find_num_paths(graph, traversed: set, current, target="out"):
     """Question is a bit unclear but my theory is
 
@@ -52,10 +130,11 @@ def find_num_paths(graph, traversed: set, current, target="out"):
     return paths
 
 
+# Naive two star
 def find_num_allowed_paths(graph, traversed: set, current, reqired, target="out"):
     """Question is a bit unclear but my theory is
 
-    Nodes - can be traversed several times
+    Nodes - can be traversed once (Try out)
     Edges - can only be traversed once!
 
     Solve with depth first
@@ -68,20 +147,52 @@ def find_num_allowed_paths(graph, traversed: set, current, reqired, target="out"
 
     if current == target:
         # Since we know required does not overlap with "out" we can:
-        nodes = {s for s, _ in traversed}
-        if (nodes & reqired) == reqired:
-            return 1
+        nodes = {s for s in traversed}
+        # if (nodes & reqired) == reqired:
+        return 1
         return 0
 
     paths = 0
 
     for next in graph.get(current, []):
-        if (current, next) not in traversed:  # not yet traversed edge
-            traversed.add((current, next))
+        if next not in traversed:  # not yet traversed node
+            traversed.add(next)
             paths += find_num_allowed_paths(graph, traversed, next, reqired, target)
-            traversed.remove((current, next))
+            traversed.remove(next)
 
     return paths
+
+
+def paths(graph, start, end, top_sorted):
+    """Using BFS
+
+    Time:  O(E)
+    Space: O(No clue)
+
+    odes - can be traversed several times
+    Edges - can only be traversed once
+
+    """
+    paths = {start: 1}
+
+    visited = set()
+    belong = set()  # belong (s) to a possible path
+    belong.add(start)
+
+    for node in top_sorted:
+        if not node in belong:
+            continue
+        next_nodes = graph.get(node, [])
+        for next in next_nodes:
+            belong.add(next)
+            # print(f"Processing {node}->{next}")
+            paths[next] = paths.get(next, 0) + paths[node]
+            if (node, next) not in visited:
+                visited.add((node, next))
+
+    # print(paths)
+
+    return paths.get(end, 0)
 
 
 input = [  # Test input for first star
@@ -113,9 +224,40 @@ input = [  # Test input for second star
     "hhh: out",
 ]
 
+# Test for sanity
+input = [
+    "svr: aaa fft bbb",
+    "aaa: fft",
+    "bbb: fft",
+    "fft: dac",
+    "dac: ccc out",
+    "ccc: out",
+    "you: out",
+]
+
 input = get_input_list(PATH)
 graph = build_graph(input)
+top_sorted = topological_sort(graph)
+
+
+# too low 368406
+# too low 1693976470344
 
 
 # print(find_num_paths(graph, set(), "you", "out"))  # First star
-# print(find_num_allowed_paths(graph, set(), "svr", {"dac", "fft"}))
+# print(find_num_allowed_paths(graph, set(), "fft", {}, "dac"))  # Second star
+
+p11 = paths(graph, "svr", "dac", top_sorted)
+p12 = paths(graph, "dac", "fft", top_sorted)
+p13 = paths(graph, "fft", "out", top_sorted)
+
+p21 = paths(graph, "svr", "fft", top_sorted)
+p22 = paths(graph, "fft", "dac", top_sorted)
+p23 = paths(graph, "dac", "out", top_sorted)
+
+
+print(p11, p12, p13)
+print(p21, p22, p23)
+
+print(p11 * p12 * p13)
+print(p21 * p22 * p23)
